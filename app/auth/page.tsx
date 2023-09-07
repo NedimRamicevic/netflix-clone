156abd511be17b7f885c5f8673dbc939f8cda62f
Nedim <mr.ramicevic@gmail.com>
2023-09-07 15:04:46 +0300

    input

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Input from '../components/Input'
 
 const Auth  = () => {
     return (
@@ -7,6 +8,16 @@ const Auth  = () => {
                 <nav className=' px-12 py-5'>
                     <Image className='' src='/images/logo.png' alt={''} width={100} height={100}  />
                 </nav>
+                <div className='flex justify-center'>
+                    <div className=' bg-black bg-opacity-70 px-12 py-16 self-center mt-2 lg:max-w-md rounded-md w-full'>
+                        <h2 className='text-white text-4xl mb-8 font-semibold' >
+                            Sign In
+                        </h2>
+                        <div className=' flex flex-col gap-4'>
+                            <Input/>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
