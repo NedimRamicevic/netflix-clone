b343b9c83affa3a9da7c667b30b9ba1437c20bca
Nedim <mr.ramicevic@gmail.com>
2023-09-09 11:32:38 +0300

    movie page

---
+++ app/components/FavoriteButton.tsx
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import React, { useCallback, useMemo } from 'react';
+import { PlusIcon, CheckIcon } from '@heroicons/react/24/outline';
+
+
+interface FavoriteButtonProps {
+  movieId: string
+}
+
+const FavoriteButton: React.FC<FavoriteButtonProps> = ({ movieId }) => {
+
+  
+
+    const Icon =  PlusIcon;
+
+  return (
+    <div onClick={()=>{}} className="cursor-pointer group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
+      <Icon className="text-white group-hover/item:text-neutral-300 w-4 lg:w-6" />
+    </div>
+  )
+}
+
+export default FavoriteButton;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import Image from 'next/image'
+import Navbar from './components/navbar'
+import MovieList from './components/MovieList'
+import Billboard from './components/Billboard'
 
 export default function Home() {
   return (
-    <div>
-       Netflix Clone App
+    <div className='flex flex-col gap-4 h-full'>
+       <Navbar />
+       <Billboard/>
+       <MovieList />
     </div>
   )
 }
